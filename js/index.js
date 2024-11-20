c8c6d0e4e6607cb2f2af8c77b80dafed0257d199
Fedecanovas <fcanovas@udesa.edu.ar>
2024-11-19 22:35:49 -0300

    Arregle el buscador

--- js/index.js
+++ js/index.js
@@ -1,1 +1,21 @@
-body
+let form = document.querySelector(".form");
+let input = document.querySelector(".input")
+let invalid = document.querySelector(".invalid")
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    let valida = true;
+    if (input.value == ""){
+        invalid.style.display = "block"
+        invalid.innerText = "El campo esta vacio"
+        valida = false
+    }
+    if (input.value.length < 3){
+        invalid.style.display = "block"
+        invalid.innerText = "Tiene que ser mayor a 3 carecteres"
+        valida = false
+    }
+    if (valida){
+        form.submit()
+    }
+})
+
